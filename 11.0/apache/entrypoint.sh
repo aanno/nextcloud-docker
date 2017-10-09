091d3928d8e2cbdebc50dd765fcc4f5120e7e07a
Thomas Pasch <thomas.pasch@gmx.de>
2017-10-09 10:24:06 +0200

    entrypoint quote fixes

--- 11.0/apache/entrypoint.sh
+++ 11.0/apache/entrypoint.sh
@@ -19,10 +19,10 @@ function directory_empty() {
 
 function run_as() {
   if [[ $EUID -eq 0 ]]; then
-    # su - www-data -s /bin/bash -c "$1"
-su - www-data -s /bin/bash <<EOSU
-$1
-EOSU
+    su - www-data -s /bin/bash -c "$1"
+# su - www-data -s /bin/bash <<EOSU
+# $1
+# EOSU
   else
     bash -c "$1"
   fi
@@ -30,7 +30,7 @@ EOSU
 
 installed_version="0.0.0~unknown"
 if [ -f ${BASE}/version.php ]; then
-    installed_version=$(php -r "require \"${BASE}/version.php\"; echo \"$OC_VersionString\";")
+    installed_version=$(php -r 'require "'${BASE}'/version.php"; echo "$OC_VersionString";')
 fi
 image_version=$(php -r 'require "/usr/src/nextcloud/version.php"; echo "$OC_VersionString";')
 
@@ -41,7 +41,7 @@ fi
 
 if version_greater "$image_version" "$installed_version"; then
     if [ "$installed_version" != "0.0.0~unknown" ]; then
-        run_as "php ${BASE}/occ app:list" > /tmp/list_before
+        run_as 'php '${BASE}'/occ app:list' > /tmp/list_before
     fi
     if [[ $EUID -eq 0 ]]; then
       rsync_options="-rlDog --chown www-data:root"
@@ -59,9 +59,9 @@ if version_greater "$image_version" "$installed_version"; then
     echo "Base is ${BASE} - it can not be changed after the first startup"
 
     if [ "$installed_version" != "0.0.0~unknown" ]; then
-        run_as "php ${BASE}/occ upgrade --no-app-disable"
+        run_as 'php '${BASE}'/occ upgrade --no-app-disable'
 
-        run_as "php ${BASE}/occ app:list" > /tmp/list_after
+        run_as 'php '${BASE}'/occ app:list' > /tmp/list_after
         echo "The following apps have beed disabled:"
         diff <(sed -n "/Enabled:/,/Disabled:/p" /tmp/list_before) <(sed -n "/Enabled:/,/Disabled:/p" /tmp/list_after) | grep '<' | cut -d- -f2 | cut -d: -f1
         rm -f /tmp/list_before /tmp/list_after
