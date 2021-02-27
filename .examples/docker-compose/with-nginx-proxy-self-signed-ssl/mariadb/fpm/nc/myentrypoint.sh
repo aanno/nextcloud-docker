bf67de244a78b8b63c3b5629f5d29f8b2364e29f
aanno <aanno@users.noreply.github.com>
2021-02-27 09:43:28 +0100

    fixes

---
+++ .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/nc/myentrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -x
+set -eu
+
+#/entrypoint.sh "$@"
+
+# NC_ROOT=/var/www/html
+# testing
+NC_ROOT=`readlink -f ./tmp`
+
+APPDATA=`ls -d $NC_ROOT/data/appdata_*`
+if [ -n "$APPDATA" ]; then
+
+    if [ "$(id -u)" = 0 ]; then
+        rsync_options="-rlDog --chown www-data:root"
+    else
+        rsync_options="-rlD"
+    fi
+    # rsync $rsync_options --delete --exclude-from=/upgrade.exclude /usr/src/nextcloud/ /var/www/html/
+
+    for dir1 in $APPDATA; do
+        for dir2 in css js; do
+            if [ -d "$dir1/$dir2" ]; then
+                rsync $rsync_options --delete \
+                "$dir1/$dir2" "$NC_ROOT"
+                echo "Updated $dir2 folder"
+            fi
+        done
+    done
+
+fi
