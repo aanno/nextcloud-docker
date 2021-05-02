31c5d94ec936a85a1db5f39d1e4b238ba951a3c8
aanno <aanno@users.noreply.github.com>
2021-05-02 10:16:03 +0000

    real certbot config

--- .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/prepare/create-dhparam.sh
+++ .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/prepare/create-dhparam.sh
@@ -4,7 +4,7 @@
 PNAME="nc"
 
 podman pod rm -f "$PNAME"
-podman rmi localhost/dhparam_certs localhost/nc_certs
+# podman rmi localhost/dhparam_certs localhost/nc_certs nc_openssl_1 || true
 podman-compose -p "$PNAME" -f create-dhparam-compose.yml up
 podman pod rm -f "$PNAME" 
-podman rmi localhost/dhparam_openssl localhost/nc_certs
+# podman rmi localhost/dhparam_openssl localhost/nc_certs nc_openssl_1 || true
