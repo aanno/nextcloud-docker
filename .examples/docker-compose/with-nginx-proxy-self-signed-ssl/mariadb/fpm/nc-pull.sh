7f7e9d5f03fc6d9e1a5c86f1b2d796cfe6938267
aanno <aanno@users.noreply.github.com>
2021-05-27 08:49:59 +0000

    nc 21.0.2
    (cherry picked from commit dab54b9a938a07d94c8bfb62de5aea2abf1fd92f)

--- .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/nc-pull.sh
+++ .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/nc-pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 podman pull nextcloud:fpm-alpine
-podman pull nextcloud:21.0.1-fpm-alpine
+podman pull nextcloud:21.0.2-fpm-alpine
 podman pull nginx:alpine
 podman pull paulczar/omgwtfssl
 podman pull redis:alpine
