2331db313fd9af4b31062bb450216a8bcbb4565d
aanno <aanno@users.noreply.github.com>
2021-07-29 11:34:37 +0200

    introduce CERTBOT_PRIMARY_DOMAIN

--- .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/scripts/nc-up.sh
+++ .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/scripts/nc-up.sh
@@ -8,16 +8,17 @@ pushd "$GIT_ROOT/.examples/docker-compose/with-nginx-proxy-self-signed-ssl/maria
 
 source "scripts/env.sh"
 
-rm web/nginx.conf || true
+rm web/nginx.conf web/snippets/le-certbot.conf || true
 # HACK for NOT replacing nginx variables
 envsubst <web/nginx.in.conf >web/nginx.conf \
   '${SERVER_FQ_DOMAINS} ${SERVER_HTTP_PORT} ${SERVER_HTTPS_PORT} ${DNS_RESOLVER_IP}'
 
+envsubst <web/snippets/le-certbot.in.conf >web/snippets/le-certbot.conf
 
-mkdir backup config apps data || true
+mkdir -p backup config apps data "${SERVER_HTTPS_CERT_VOLUME}/live/${CERTBOT_PRIMARY_DOMAIN}" || true
 
 pushd prepare
-  #./create-dhparam.sh
+  ./create-dhparam.sh
 popd
 
 podman rmi localhost/nc_web localhost/fpm_web localhost/nc_proxy localhost/fpm_proxy || true
