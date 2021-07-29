7c866ca46e9974f1cae87b0aa47ab7cbfbf7ca75
aanno <aanno@users.noreply.github.com>
2021-07-29 10:42:52 +0200

    more nginx port configurable

--- .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/scripts/nc-up.sh
+++ .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/scripts/nc-up.sh
@@ -9,13 +9,18 @@ pushd "$GIT_ROOT/.examples/docker-compose/with-nginx-proxy-self-signed-ssl/maria
 source "scripts/env.sh"
 
 rm web/nginx.conf || true
-envsubst <web/nginx.in.conf >web/nginx.conf
+# HACK for NOT replacing nginx variables
+envsubst <web/nginx.in.conf >web/nginx.conf \
+  '${SERVER_FQ_DOMAINS} ${SERVER_HTTP_PORT} ${SERVER_HTTPS_PORT} ${DNS_RESOLVER_IP}'
+
+
+mkdir backup config apps data || true
 
 pushd prepare
-  ./create-dhparam.sh
+  #./create-dhparam.sh
 popd
 
-podman rmi localhost/nc_web localhost/fpm_web localhost/nc_proxy localhost/fpm_proxy 
-# podman-compose -p nc up -d 
+podman rmi localhost/nc_web localhost/fpm_web localhost/nc_proxy localhost/fpm_proxy || true
+podman-compose -p nc up # -d 
 
 popd
