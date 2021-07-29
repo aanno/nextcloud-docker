3756b9b943543c0f6dfed7abe788dfe96c9fe1a4
aanno <aanno@users.noreply.github.com>
2021-07-29 14:04:56 +0200

    create dummy cert fixed

--- .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/prepare/create-dummy-cert.sh
+++ .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/prepare/create-dummy-cert.sh
@@ -7,7 +7,7 @@ pushd "$GIT_ROOT/.examples/docker-compose/with-nginx-proxy-self-signed-ssl/maria
 
 source "scripts/env.sh"
 
-export SERVER_DOMAINS_COMMA=`echo ${SERVER_FQ_DOMAINS} | sed -E 's/[[:space:]]+/,/g' | cat`
+export SERVER_DOMAINS_COMMA=`echo ${SERVER_FQ_DOMAINS} | sed -E 's/[[:space:]]+/,/g'`
 
 # ATTENTION: pom MUST be named 'nc' - otherwise the volume has the wrong name!
 PNAME="nc"
@@ -16,6 +16,14 @@ pushd prepare
   podman pod rm -f "$PNAME" || true
   podman-compose -p "$PNAME" -f create-dummy-cert-compose.yml up
   podman pod rm -f "$PNAME" 
+popd
+
+# TODO tp: Volume must be absolute path for this, need to done in container (if done right)
+pushd "${SERVER_HTTPS_CERT_VOLUME}/live/${CERTBOT_PRIMARY_DOMAIN}"
+  ln -sf "../../cert.pem" fullchain.pem
+  ln -sf "../../key.pem" privkey.pem
+  cd "../.."
+  tree -a .
 popd
 
 popd
