8637710f20342afb4b7860beb5c2bb90b4e4dff7
aanno <aanno@users.noreply.github.com>
2022-01-06 19:07:21 +0100

    stick to mariadb:10.6

--- .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/scripts/nc-pull.sh
+++ .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/scripts/nc-pull.sh
@@ -5,6 +5,8 @@ podman pull nextcloud:23.0.0-fpm-alpine
 podman pull nginx:alpine
 podman pull paulczar/omgwtfssl
 podman pull redis:alpine
-podman pull mariadb
+podman pull mariadb:10.6.5
+podman pull mariadb:10.7.1
+podman pull mariadb:latest
 podman pull docker.io/aanno/notify_push:v0.3.0
 podman pull aanno/certbot-netcup:v1.21.0
