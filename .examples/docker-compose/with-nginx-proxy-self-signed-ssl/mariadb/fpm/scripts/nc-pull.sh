d735ea6669539fe9e5c95bc27625f0aec010918d
aanno <aanno@users.noreply.github.com>
2022-02-19 12:43:14 +0100

    nextcloud 23.0.2

--- .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/scripts/nc-pull.sh
+++ .examples/docker-compose/with-nginx-proxy-self-signed-ssl/mariadb/fpm/scripts/nc-pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 podman pull nextcloud:fpm-alpine
-podman pull nextcloud:23.0.0-fpm-alpine
+podman pull nextcloud:23.0.2-fpm-alpine
 podman pull nginx:alpine
 podman pull paulczar/omgwtfssl
 podman pull redis:alpine
@@ -9,4 +9,4 @@ podman pull mariadb:10.6
 podman pull mariadb:10.7
 podman pull mariadb:latest
 podman pull docker.io/aanno/notify_push:v0.3.0
-podman pull aanno/certbot-netcup:v1.21.0
+podman pull aanno/certbot-netcup:v1.23.0
